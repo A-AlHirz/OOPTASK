283be7e50704784d5adaba978909433ee7d51114
A-AlHirz <abdherz@hotmail.com>
2024-12-24 14:45:34 +0300

    Add project files.

---
+++ OOPtask/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPtask.Models
+{
+    public class Student
+    {
+        public string Name;
+        public string Major;
+        public double GPA;
+
+        public string GetInfo(string name, string major, double gpa)
+        {
+            string result;
+            result = $"Name: {name}, Major: {major}, GPA: {gpa}";
+            return result;
+
+        }
+    }
+}
---
+++ OOPtask/Program.cs
@@ -0,0 +1,17 @@
+﻿using OOPtask.Models;
+
+Student std = new Student();
+
+string name, major;
+double gpa;
+
+Console.WriteLine("Name: ");
+name = Console.ReadLine();
+
+Console.WriteLine("Major: ");
+major = Console.ReadLine();
+
+Console.WriteLine("GPA: ");
+gpa = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(std.GetInfo(name, major, gpa));
