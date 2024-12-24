f2e03742d41ce8316a11d17c5910b692f17d2375
A-AlHirz <abdherz@hotmail.com>
2024-12-24 15:36:34 +0300

    Student&Graduate

---
+++ OOPtask/Models/Graduatestudent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPtask.Models
+{
+    internal class Graduatestudent : Student
+    {
+    }
+}
--- OOPtask/Program.cs
+++ OOPtask/Program.cs
@@ -1,17 +1,39 @@
 ﻿using OOPtask.Models;
-
+#region Student
 Student std = new Student();
 
 string name, major;
 double gpa;
 
-Console.WriteLine("Name: ");
+Console.Write("Name: ");
 name = Console.ReadLine();
 
-Console.WriteLine("Major: ");
+Console.Write("Major: ");
 major = Console.ReadLine();
 
-Console.WriteLine("GPA: ");
+Console.Write("GPA: ");
 gpa = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine(std.GetInfo(name, major, gpa));
+#endregion
+
+Console.WriteLine("--------------------------");
+
+#region Graduatestudent
+Graduatestudent Gstd = new Graduatestudent();
+
+string Gname, Gmajor;
+double Ggpa;
+
+Console.Write("\n Graduate Name: ");
+Gname = Console.ReadLine();
+
+Console.Write("Major: ");
+Gmajor = Console.ReadLine();
+
+Console.Write("GPA: ");
+Ggpa = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(Gstd.GetInfo(Gname, Gmajor, Ggpa));
+
+#endregion
